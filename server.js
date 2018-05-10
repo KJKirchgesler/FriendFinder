fcd6e241aa9affbaba05a3cb254b4d9c745d5972
Kristen Kirchgesler <kristen.arbutiski@gmail.com>
2018-05-10 14:31:47 -0400

    deleted survey.js and fixed bugs in survey.html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependences - Required npm packages
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 // Express configuration
 // Tells node this is an "express" server
@@ -10,8 +11,16 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
 
 // Router - points the server to api and html route files.
 
